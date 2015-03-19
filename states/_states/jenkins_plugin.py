2c01528e5ccc415bc3abc8f454a3b5a00a3eae2e
Stefano Pogliani <stefano@spogliani.net>
2015-03-19 17:49:55 +0000

    First version of Jenkins plugin downloader.

--- states/_states/jenkins_plugin.py
+++ states/_states/jenkins_plugin.py
@@ -5,27 +5,60 @@ State module to install Jenkins Plugins.
 
 # Import python modules.
 from __future__ import absolute_import
+import json
 import logging
-
-# Import salt modules.
-from salt.utils import http
+import os
 
 
-_plugin_cache = None
+# Import external modules.
+import requests
+
+
 log = logging.getLogger(__name__)
 
-_PLUGINS_URL="https://updates.jenkins-ci.org/update-center.json"
+
+class PluginCache(object):
+  PLUGINS_URL="https://updates.jenkins-ci.org/update-center.json"
+
+  def __init__(self):
+    super(PluginCache, self).__init__()
+    self._cache = None
+
+  def _update(self, force=False):
+    if force or self._cache is None:
+      log.info("Downloading plugin information")
+      # Fetch the JSONP file.
+      data = requests.get(PluginCache.PLUGINS_URL).text
+      data = data.split("\n")[1]
+      self._cache = json.loads(data)["plugins"]
+
+  def download(self, name, target):
+    self._update()
+    if name not in self._cache:
+      raise Exception("Cannot download inexisting plugin")
+
+    location = self._cache[name]["url"]
+    log.debug("Downloading Jenkins Plugin {name} from {url}".format(
+      name=name, url=location
+    ))
+
+    stream = requests.get(location, stream=True)
+    with open(target, "wb") as f:
+      for chunk in stream.iter_content(chunk_size=1024):
+        if chunk: # filter out keep-alive new chunks
+          f.write(chunk)
+          f.flush()
+
+  def get_latest_version(self, name):
+    self._update()
+    if name in self._cache:
+      return self._cache[name]["version"]
+    return None
+
+_cache = PluginCache()
 
 
-def _get_latest_version(name):
-  if _plugin_cache is None:
-    log.info("Downloading plugin information")
-    _plugin_cache = http.query(_PLUGINS_URL)
-
-  log.debug(("~~~", _plugin_cache)
-
-
-def ensure(name=None, update=True, *args, **kwargs):
+def ensure(home="/var/lib/jenkins", name=None, update=True, **kwargs):
   result = {
     "changes": {},
     "comment": "",
@@ -34,15 +67,41 @@ def ensure(name=None, update=True, *args, **kwargs):
   }
 
   # Check if plugin is available and fetch the version.
-  latest_version = _get_latest_version(name)
+  latest_version = _cache.get_latest_version(name)
   log.debug("Found plugin {name}, version {version}".format(
-    name=name, version-latest_version
+    name=name, version=latest_version
   ))
 
   # Check if plugin is installed.
-  # If it is, figure out the installed version.
-  # Install update if needed and requested.
+  download_target   = os.path.join(home, "plugins", name + ".hpi")
+  metadata_location = os.path.join(
+      home, "plugins", name, "META-INF", "MANIFEST.MF"
+  )
+  installed = os.exists(metadata_location)
+  outdated  = False
+  version   = None
 
-  log.debug(("~~~", args, kwargs))
+  # If it is, figure out the installed version.
+  if installed:
+    with open(metadata_location, "r") as metadata:
+      version = [
+          line for line in metadata.readlines()
+          if line.startswith("Plugin-Version: ")
+      ][0][15:]
+      outdated = version != latest_version
+      log.debug("Detected installed version {version}".format(version=version))
+
+  # Install update if needed and requested.
+  if not installed or (update and outdated):
+    _cache.download(name, download_target)
+    result["changes"][name] = {
+      "new": latest_version,
+      "old": version
+    }
+
+  else:
+    change = {}
+
+  log.debug(("~~~", kwargs))
   return result
 
