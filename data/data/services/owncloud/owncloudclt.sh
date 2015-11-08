8753107b45b4a479201e92d35b1260d633c73d49
Stefano Pogliani <stefano@spogliani.net>
2015-11-08 11:25:23 +0000

    Check if owncloud is installed.

--- data/data/services/owncloud/owncloudclt.sh
+++ data/data/services/owncloud/owncloudclt.sh
@@ -4,7 +4,13 @@
 
 # Constants.
 EXIT_INVALID_COMMAND=1
-EXIT_WRONG_USER=2
+EXIT_NOT_INSTALLED=2
+EXIT_OK=3
+EXIT_WRONG_USER=4
+
+HTTP_USER="www-data"
+OWNCLOUD_ROOT="/var/www/owncloud"
+OWNCLOUD_CTL="./occ"
 
 
 # Functions.
@@ -24,10 +30,34 @@ COMMAND:
 }
 
 
+# Commans.
+check() {
+  what=$1
+  case "${what}" in
+    install) check_install;;
+    *)
+      echo "Unkonw check argument: '${what}'."
+      show_help
+      exit ${EXIT_INVALID_COMMAND}
+      ;;
+  esac
+}
+
+check_install() {
+  cd ${OWNCLOUD_ROOT}
+  installed=${OWNCLOUD_CTL} list | grep -c 'ownCloud is not installed'
+  if [ "${installed}" -eq 1 ]; then
+    exit ${EXIT_NOT_INSTALLED}
+  else
+    exit ${EXIT_OK}
+  fi
+}
+
+
 ### MAIN ###
 # Check pre-conditions.
-if [ "$(whoami)" != "www-data" ]; then
-  echo "This script must run as the www-data user."
+if [ "$(whoami)" != "${HTTP_USER}" ]; then
+  echo "This script must run as the ${HTTP_USER} user."
   exit ${EXIT_WRONG_USER}
 fi
 
@@ -37,9 +67,11 @@ cmd=$1
 shift
 
 case "${cmd}" in
-  help) show_help;;
+  check) check $*;;
+  help)  show_help;;
   *)
     echo "Unsupported command: '${cmd}'."
     show_help
     exit ${EXIT_INVALID_COMMAND}
+    ;;
 esac
